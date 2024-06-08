9a72dc4d6df38e0bf92fc9410a7ebb21255c642d
Aaron Hsu <aahsu812@gmail.com>
2024-06-08 00:42:46 -0400

    home test

--- tests/test1.py
+++ tests/test1.py
@@ -28,5 +28,4 @@ for course in course_cards:
     course_price = course.a.text.split()[-1] # a tag stores the prices 
 
     print(f'{course_name} costs {course_price}')
-
-#seth is gay 
+    print(course.text)
